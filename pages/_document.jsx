bd3c0f7d372b502eda2c1cdac2c83111ed24b1b1
Mugven <155220144+Mugven@users.noreply.github.com>
2023-12-30 13:59:20 +0300

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,29 +11,29 @@ class MyDocument extends Document {
           <meta name="googlebot" content="index,follow" />
           <meta
             name="description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="Selam ben Moderasyon ve Eğlence botuyum sunucuna eklemek veya destek almak için websitemi ziyaret et!"
           />
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:site" content="@MusicMaker" />
-          <meta name="twitter:creator" content="@MusicMaker" />
-          <meta property="og:url" content="https://musicmaker.vercel.app" />
+          <meta name="twitter:creator" content="@MuG" />
+          <meta property="og:url" content="https://mug-bot-mu-g.vercel.app/" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="MusicMaker" />
+          <meta property="og:title" content="MuG" />
           <link
             rel="icon"
-            href="/img/logo2.png"
+            href=""
             type="image/x-icon"
           />
           <meta
             property="og:description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="Selam ben Moderasyon ve Eğlence botuyum sunucuna eklemek veya destek almak için websitemi ziyaret et!"
           />
           <meta property="og:image" content="/img/logo.jpg" />
-          <meta property="og:image:alt" content="MusicMaker" />
+          <meta property="og:image:alt" content="MuG" />
           <meta property="og:locale" content="en_GB" />
           <meta
             property="og:site_name"
-            content="MusicMaker"
+            content="MuG"
           />
           <meta name="theme-color" content="#087ddd" />
           <link rel="icon" href="/img/logo.jpg" type="image/x-icon" />
