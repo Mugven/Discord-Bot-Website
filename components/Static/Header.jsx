b3a8871a7fdf9edd647d8213e2e9ab9b61a3fdb6
Mugven <155220144+Mugven@users.noreply.github.com>
2023-12-30 16:33:05 +0300

    Update Header.jsx

--- components/Static/Header.jsx
+++ components/Static/Header.jsx
@@ -166,9 +166,9 @@ const Header = ({ $, NavItems }) => {
                                 </Menu.Items>
                             </Transition>
                         </Menu>
-                        <Link href="https://discord.com/api/oauth2/authorize?client_id=774043716797071371&permissions=277028620608&scope=applications.commands%20bot">
+                        <Link href="https://discordapp.com/oauth2/authorize?client_id=1187728080249307156&scope=bot&permissions=2146958847">
                             <a className="w-auto flex items-center justify-center shadow-lg gap-x-2 shadow-amber-600/20 rounded-xl py-2.5 font-medium px-7 bg-gradient-to-tl from-amber-500 to-amber-700 text-white  hover:opacity-80 transition duration-200">
-                               Invite
+                               Davet ET
                             </a>
                </Link>
                     </div>
