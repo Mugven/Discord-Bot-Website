2d48846b787ab38086531acb85136901626d1158
Mugven <155220144+Mugven@users.noreply.github.com>
2024-02-15 21:32:13 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -5,7 +5,7 @@ export default function Privacy() {
             <h1 className="font-semibold text-3xl text-white/75">Gizlilik Politikası MuG</h1>
             <p className="text-sm text-white/30 mb-4">Son Güncelleme: 30/12/2023  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ MusicMaker botunu kullanmak için discord sunucunuza eklerken gerekli izinleri tanımlamanız gerekir. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
+        <p>⊳ MuG botunu kullanmak için discord sunucunuza eklerken gerekli izinleri tanımlamanız gerekir. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
         <div className="py-3"></div>
         <p>⊳ için oy vermelisiniz <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">MuG</a> botu, bot komutlarından belirli komutları kullanırken.</p>
         <div className="py-3"></div>
@@ -13,7 +13,7 @@ export default function Privacy() {
         <div className="py-3"></div>
         <p>⊳ MusicMaker'ı kullanırken belirli komutları aynı anda kullanamazsınız.</p>   
         <div className="py-3"></div>
-        <p>⊳ MusicMaker botu kullanıcılara dayalı hiçbir veriyi kaydetmez.</p>
+        <p>⊳ MuG botu kullanıcılara dayalı hiçbir veriyi kaydetmez.</p>
         <div className="py-3"></div>
         <p>⊳ Botu kullanırken kullanıcı kimliğiniz bir defaya mahsus olmak üzere 60 saniye süreyle sisteme kaydedilip silinir.</p> 
     
